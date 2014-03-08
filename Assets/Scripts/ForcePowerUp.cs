a166a19e7b9b3b5a7ee8aa888ca33ce2a5c8d70b
davesimd <dave.simd@gmail.com>
2014-03-07 22:30:44 -0500

    Inventory v2

--- Assets/Scripts/ForcePowerUp.cs
+++ Assets/Scripts/ForcePowerUp.cs
@@ -3,6 +3,13 @@ using System.Collections;
 
 public class ForcePowerUp : MonoBehaviour {
 
+	//public GameObject otherGameObject;
+	private PlayerInventoryPowerUp playerInventory;
+
+	void Awake() {
+		//playerInventory = otherGameObject.GetComponent<PlayerInventoryPowerUp>();
+	}
+
 	// Use this for initialization
 	void Start () {
 	
@@ -10,6 +17,16 @@ public class ForcePowerUp : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+
+	}
+
+	void OnCollisionEnter2D (Collision2D col)
+	{
+		if(col.gameObject.name == "Ground")
+		{
+			playerInventory = col.gameObject.GetComponent<PlayerInventoryPowerUp>();
+			playerInventory.AddPowerUp(gameObject);
+			Destroy(gameObject);
+		}
 	}
 }
