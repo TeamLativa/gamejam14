210460e8b0c0650a7ca43a31f27f47a8e7489e32
dada9109 <david.gagne0@gmail.com>
2014-03-08 14:36:58 -0500

    mis les vrai sprites sur le totem pi y flip

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -76,52 +76,60 @@ public class PlayerController : MonoBehaviour {
 			if ((collision.gameObject.tag == "Ground") || (collision.gameObject.tag == "PlatformTop")
 			    || (collision.gameObject.tag == "PlatformTotem")) 
 			{
-				//
-				if(Input.GetButtonDown("Xbutton_"+PNumber))
+				if(collision.gameObject.tag == "PlatformTotem")
 				{
-					//add things to totem
-					//if(player got all items to add a part)
-
-					switch (partNumber)
+					if(Input.GetButtonDown("Xbutton_"+PNumber))
 					{
-					case 0 : break; 
-					case 1 : 
-						Instantiate(TotemPart1, TotemPart1.transform.position, Quaternion.identity);
-						partNumber = 0;
-						Instantiate(TotemPart1, new Vector2(-TotemPart1.transform.position.x,TotemPart1.transform.position.y), Quaternion.identity);
-						Instantiate(TotemPart2, new Vector2(-TotemPart2.transform.position.x,TotemPart2.transform.position.y), Quaternion.identity);
-						Instantiate(TotemPart3, new Vector2(-TotemPart3.transform.position.x,TotemPart3.transform.position.y), Quaternion.identity);
-						Instantiate(TotemPart4, new Vector2(-TotemPart4.transform.position.x,TotemPart4.transform.position.y), Quaternion.identity);
-						Instantiate(TotemPart5, new Vector2(-TotemPart5.transform.position.x,TotemPart5.transform.position.y), Quaternion.identity);
-						Instantiate(TotemPart6, new Vector2(-TotemPart6.transform.position.x,TotemPart6.transform.position.y), Quaternion.identity);
+						//add things to totem
+						//if(player got all items to add a part)
 
-						//load gameOver when its gonna be made
-						Application.LoadLevel ("level");
+						switch (partNumber)
+						{
+						case 0 : break; 
+						case 1 : 
+							Instantiate(TotemPart1, TotemPart1.transform.position, Quaternion.identity);
+							partNumber = 0;
 
-						break;
-					case 2 : 
-						Instantiate(TotemPart2, TotemPart2.transform.position, Quaternion.identity);
-						partNumber = 1;
-						break;
-					case 3 : 
-						Instantiate(TotemPart3, TotemPart3.transform.position, Quaternion.identity);
-						partNumber = 2;
-						break;
-					case 4 : 
-						Instantiate(TotemPart4, TotemPart4.transform.position, Quaternion.identity);
-						partNumber = 3;
-						break;
-					case 5 : 
-						Instantiate(TotemPart5, TotemPart5.transform.position, Quaternion.identity);
-						partNumber = 4;
-						break;
-					case 6 : 
-						Instantiate(TotemPart6, TotemPart6.transform.position, Quaternion.identity);
-						partNumber = 5;
-						break;
+							GameObject part;
+							part = (GameObject)Instantiate(TotemPart6, new Vector2(-TotemPart6.transform.position.x,TotemPart6.transform.position.y), Quaternion.identity);
+							part.transform.localScale = Flip (part.transform.localScale);
+							part = (GameObject)Instantiate(TotemPart5, new Vector2(-TotemPart5.transform.position.x,TotemPart5.transform.position.y), Quaternion.identity);
+							part.transform.localScale = Flip (part.transform.localScale);					
+							part = (GameObject)Instantiate(TotemPart4, new Vector2(-TotemPart4.transform.position.x,TotemPart4.transform.position.y), Quaternion.identity);
+							part.transform.localScale = Flip (part.transform.localScale);
+							part = (GameObject)Instantiate(TotemPart3, new Vector2(-TotemPart3.transform.position.x,TotemPart3.transform.position.y), Quaternion.identity);
+							part.transform.localScale = Flip (part.transform.localScale);
+							part = (GameObject)Instantiate(TotemPart2, new Vector2(-TotemPart2.transform.position.x,TotemPart2.transform.position.y), Quaternion.identity);
+							part.transform.localScale = Flip (part.transform.localScale);
+							part = (GameObject)Instantiate(TotemPart1, new Vector2(-TotemPart1.transform.position.x,TotemPart1.transform.position.y), Quaternion.identity);
+							part.transform.localScale = Flip (part.transform.localScale);
+
+							//load gameOver when its gonna be made
+							//Application.LoadLevel ("level");
+
+							break;
+						case 2 : 
+							Instantiate(TotemPart2, TotemPart2.transform.position, Quaternion.identity);
+							partNumber = 1;
+							break;
+						case 3 : 
+							Instantiate(TotemPart3, TotemPart3.transform.position, Quaternion.identity);
+							partNumber = 2;
+							break;
+						case 4 : 
+							Instantiate(TotemPart4, TotemPart4.transform.position, Quaternion.identity);
+							partNumber = 3;
+							break;
+						case 5 : 
+							Instantiate(TotemPart5, TotemPart5.transform.position, Quaternion.identity);
+							partNumber = 4;
+							break;
+						case 6 : 
+							Instantiate(TotemPart6, TotemPart6.transform.position, Quaternion.identity);
+							partNumber = 5;
+							break;
+						}					
 					}
-
-				
 				}
 
 				if(Input.GetButtonDown("Abutton_"+PNumber))
@@ -133,6 +141,12 @@ public class PlayerController : MonoBehaviour {
 		}
 	}
 
+	Vector3 Flip(Vector3 currentScale){
+		Vector3 scale = currentScale;
+		scale.x *= -1;
+		return scale;
+	}
+	
 	public bool IsStunned(){
 		return stunned;
 	}
