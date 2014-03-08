cae1c9f3299933834568b587c5279ae7ebc3feff
MarcDufresne <madmoi23@gmail.com>
2014-03-07 19:21:13 -0500

    Added graybox, but with more colors

---
+++ Assets/Scripts/GridSystem.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class GridSystem : MonoBehaviour {
+
+	public float snap = 1f;
+	public float depth = 0;
+	
+	// Update is called once per frame
+	void Update () {
+		float inverseSnap = 1 / snap;
+		float x, y, z;
+
+		x = Mathf.Round (transform.localPosition.x * inverseSnap) / inverseSnap;
+		y = Mathf.Round (transform.localPosition.y * inverseSnap) / inverseSnap;
+		z = depth;
+
+		transform.localPosition = new Vector3 (x, y, z);
+	}
+}
