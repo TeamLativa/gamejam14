c216dca19e36dab516ad65faae9690aa0e015452
MarcDufresne <madmoi23@gmail.com>
2014-03-08 01:15:40 -0500

    New Level

--- Assets/Scripts/Enemies/LandEnemy.cs
+++ Assets/Scripts/Enemies/LandEnemy.cs
@@ -1,35 +1,34 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class FlyingEnemy : MonoBehaviour {
-
+public class LandEnemy : MonoBehaviour {
+	
 	// Life
 	public int HealthPoints = 100;
 	public GameObject DeathParticule;
-
+	
 	// Movement
 	public float MovementSpeed = 3.0f;
-	public Transform[] WaypointSets = new Transform[3];
-	private Transform waypoints;
-	private int nextWaypoint = 0;
-	private int currentWaypoint = 0;
-
+	public float Force = 15.0f;
+	public Transform SpawnPoints;
+	private Vector2 direction;
+	private GameObject lastCollidedWall;
+	
 	// Firing
 	public float FireRate = 3.0f;
 	private float fireTimer = 0.0f;
 	public GameObject Projectile;
-
+	
 	// Item Drop
 	public GameObject[] Items = new GameObject[2];
 	public GameObject[] PowerUps = new GameObject[3];
 	public float ItemDropRate = 10.0f;
 	public float PowerUpDropRate = 80.0f;
-
+	
 	// Use this for initialization
 	void Start () {
-		waypoints = (Transform) WaypointSets[Random.Range(0, WaypointSets.GetLength(0))];
-		nextWaypoint = Random.Range(0,waypoints.childCount);
-		transform.position = waypoints.GetChild(nextWaypoint).transform.position;
+		direction = transform.forward + new Vector3(1.0f, 0.0f);
+		transform.position = SpawnPoints.GetChild(Random.Range(0, SpawnPoints.childCount)).position;
 	}
 	
 	// Update is called once per frame
@@ -41,39 +40,35 @@ public class FlyingEnemy : MonoBehaviour {
 	void FixedUpdate(){
 		HandleMovement();
 	}
-
+	
 	void HandleMovement(){
-		Transform targetWaypointLocation = waypoints.GetChild(nextWaypoint);
-		Vector3 relative = targetWaypointLocation.position - transform.position;
-		Vector3 movementNormal = Vector3.Normalize(relative);
-		float distanceToWaypoint = relative.magnitude;
-
-		if(distanceToWaypoint < 0.1f){
-			currentWaypoint = nextWaypoint;
-			if(currentWaypoint <= 2){
-				nextWaypoint = Random.Range(3,6);
-			}
-			else{
-				nextWaypoint = Random.Range(0,3);
-			}
-		} else {
-			transform.position += new Vector3(movementNormal.x, movementNormal.y) * MovementSpeed * Time.deltaTime;
-		}
+		rigidbody2D.velocity = new Vector2(MovementSpeed * direction.x, rigidbody2D.velocity.y);
+		Debug.Log(direction);
 	}
 
+	void OnCollisionEnter2D(Collision2D coll){
+		if(coll.gameObject.tag == "Wall" || coll.gameObject.tag == "PlatformSide"){
+			if(coll.gameObject != lastCollidedWall){
+				lastCollidedWall = coll.gameObject;
+				Debug.Log ("Collided, changing direction");
+				direction = -direction;
+			}
+		}
+	}
+	
 	void HandleFire(){
 		fireTimer += Time.deltaTime;
-
+		
 		if(fireTimer >= FireRate){
-			Instantiate(Projectile, transform.position, Quaternion.identity);
+			// Fire towards player
 			fireTimer = 0.0f;
 		}
 	}
-
+	
 	void TakeDamage(int amount){
 		HealthPoints -= amount;
 	}
-
+	
 	void HandleDeath(){
 		if(HealthPoints <= 0){
 			// Maybe notify someone
@@ -82,10 +77,10 @@ public class FlyingEnemy : MonoBehaviour {
 			Destroy(gameObject);
 		}
 	}
-
+	
 	void DropItem(){
 		float chance = Random.Range(0,100.0f);
-
+		
 		if(chance < ItemDropRate){
 			//Generate random number to decide the item to drop
 			//Drop the item
