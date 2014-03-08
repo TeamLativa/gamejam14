20e0906d2824b2b11f27c6ec58abb3c26d9eba7d
dada9109 <david.gagne0@gmail.com>
2014-03-07 22:25:20 -0500

    modified playController.. still without jump

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,73 +1,68 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-[RequireComponent(typeof(PlayerPhysics))]
+[RequireComponent(typeof(Rigidbody2D))]
 public class PlayerController : MonoBehaviour {
 
-	public float Gravity = 10;
-	public float Speed = 1;
-	public float Acceleration = 10;
-	public float JumpHeight = 12;
+	public float Speed = 10f;
+	public float JumpHeight = 10f;
 	
 	private float currentSpeed;
 	private float targetSpeed;
 	private Vector2 amountToMove;
-	
-	private PlayerPhysics playerPhysics;
-	
-	
+
+	[HideInInspector]
+	public bool Grounded = false;
+
 	// Use this for initialization
 	void Start () {
-		playerPhysics = GetComponent<PlayerPhysics> ();
+
 	}
-	
+
+	void Update()
+	{
+
+	}
+
 	// Update is called once per frame
-	void Update () {
-
-		if (playerPhysics.movementStopped)
+	void FixedUpdate () {
+		if(Grounded)
 		{
-			targetSpeed = 0;
-			currentSpeed = 0;
-		}
-
-		if(playerPhysics.grounded)
-		{
-			amountToMove.y = 0;
-
 			//Jump
 			if(Input.GetButtonDown("Jump"))
 			{
 				amountToMove.y = JumpHeight;
 			}
+			else
+				amountToMove.y = 0;
+
 			//Fire Gun
 			if(Input.GetButtonDown("Fire1"))
 			{
 				//Shoot gun
 			}
 		}
-
 		//Input
 		targetSpeed = Input.GetAxis ("Horizontal") * Speed;
-		currentSpeed = incrementTowards (currentSpeed, targetSpeed, Acceleration);
 
 		//Set Amount to move
-		amountToMove.x = currentSpeed; 
-		amountToMove.y -= Gravity * Time.deltaTime;
-		playerPhysics.Move (amountToMove * Time.deltaTime);
-
-
-
+		amountToMove.x = targetSpeed; 
+		rigidbody2D.AddForce(amountToMove);
 	}
 
-	private float incrementTowards(float n, float target, float a) {
-		if (n == target) {
-			return n;	
-		}
-		else {
-			float dir = Mathf.Sign(target - n); // must n be increased or decreased to get closer to target
-			n += a * Time.deltaTime * dir;
-			return (dir == Mathf.Sign(target-n))? n: target; // if n has now passed target then return target, otherwise return n
+	void OnCollisionEnter2D(Collision2D collision)
+	{
+		if ((collision.gameObject.tag == "Ground") || (collision.gameObject.tag == "Platform")) 
+		{
+			Grounded = true;
 		}
 	}
 
+	void OnCollisionExit2D(Collision2D collision)
+	{
+		if ((collision.gameObject.tag == "Ground") || (collision.gameObject.tag == "Platform")) 
+		{
+			Grounded = false;
+		}
+	}
 }
