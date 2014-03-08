5091f4bc3c46a88a3297c109cbb58667f6aa6049
dada9109 <david.gagne0@gmail.com>
2014-03-08 12:20:21 -0500

    changed the design of the totem, it can be build and end the game.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -12,7 +12,16 @@ public class PlayerController : MonoBehaviour {
 	private float currentSpeed;
 	private float targetSpeed;
 	private Vector2 amountToMove;
-	
+
+	public GameObject TotemPart1;
+	public GameObject TotemPart2;
+	public GameObject TotemPart3;
+	public GameObject TotemPart4;
+	public GameObject TotemPart5;
+	public GameObject TotemPart6;
+
+	private int partNumber = 6;
+
 	private bool stunned = false;
 	private float stunnedTimer = 0.0f;
 
@@ -67,8 +76,45 @@ public class PlayerController : MonoBehaviour {
 				{
 					//add things to totem
 					//if(player got all items to add a part)
-					//instantiate(blueTotem, blueTotem.transform.position, Quaternion.identity);
-					Debug.Log ("IM adding parts to the totem!");
+
+					switch (partNumber)
+					{
+					case 0 : break; 
+					case 1 : 
+						Instantiate(TotemPart1, TotemPart1.transform.position, Quaternion.identity);
+						partNumber = 0;
+						Instantiate(TotemPart1, new Vector2(-TotemPart1.transform.position.x,TotemPart1.transform.position.y), Quaternion.identity);
+						Instantiate(TotemPart2, new Vector2(-TotemPart2.transform.position.x,TotemPart2.transform.position.y), Quaternion.identity);
+						Instantiate(TotemPart3, new Vector2(-TotemPart3.transform.position.x,TotemPart3.transform.position.y), Quaternion.identity);
+						Instantiate(TotemPart4, new Vector2(-TotemPart4.transform.position.x,TotemPart4.transform.position.y), Quaternion.identity);
+						Instantiate(TotemPart5, new Vector2(-TotemPart5.transform.position.x,TotemPart5.transform.position.y), Quaternion.identity);
+						Instantiate(TotemPart6, new Vector2(-TotemPart6.transform.position.x,TotemPart6.transform.position.y), Quaternion.identity);
+
+						//load gameOver when its gonna be made
+						Application.LoadLevel ("level");
+
+						break;
+					case 2 : 
+						Instantiate(TotemPart2, TotemPart2.transform.position, Quaternion.identity);
+						partNumber = 1;
+						break;
+					case 3 : 
+						Instantiate(TotemPart3, TotemPart3.transform.position, Quaternion.identity);
+						partNumber = 2;
+						break;
+					case 4 : 
+						Instantiate(TotemPart4, TotemPart4.transform.position, Quaternion.identity);
+						partNumber = 3;
+						break;
+					case 5 : 
+						Instantiate(TotemPart5, TotemPart5.transform.position, Quaternion.identity);
+						partNumber = 4;
+						break;
+					case 6 : 
+						Instantiate(TotemPart6, TotemPart6.transform.position, Quaternion.identity);
+						partNumber = 5;
+						break;
+					}
 				}
 			}
 
