9c0ee2c769a2c76bd1f33a3f9b6e37b9145a512d
MarcDufresne <madmoi23@gmail.com>
2014-03-07 19:56:37 -0500

    Havin' fun

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour {
+
+	public int HealthPoints = 100;
+	public float MovementSpeed = 3.0f;
+
+	bool goingForward = true;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void TakeDamage(int amount){
+		HealthPoints -= amount;
+	}
+
+	void OnCollisionEnter2D(Collision2D coll){
+		if(coll.gameObject.tag
+	}
+}
