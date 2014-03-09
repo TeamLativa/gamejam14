d8a0f46a309d0346943028aa1625f5d36c707c2c
dada9109 <david.gagne0@gmail.com>
2014-03-08 19:57:48 -0500

    totem completed

--- Assets/Scripts/PlayerInventoryMaterials.cs
+++ Assets/Scripts/PlayerInventoryMaterials.cs
@@ -4,13 +4,24 @@ using System.Collections.Generic;
 
 public class PlayerInventoryMaterials : MonoBehaviour {
 
-	private int materialBois;
-	private int materialRoche;
-	private int materialCorde;
+	public GameObject GuiRoche,GuiBois,GuiOs,GuiMetal,GuiPlume,GuiLiane;
+
+	private int nbItem,nbRoche, nbBois,nbOs,nbMetal,nbPlume,nbLiane;
+
+	private GUITexture rendererRoche,rendererBois, rendererOs, rendererMetal, rendererPlume, rendererLiane;
+
+	private GUIText texteRoche,texteBois, texteOs, texteMetal, textePlume, texteLiane;
+
+	private int materialRoche,materialBois,materialOs,materialMetal,materialPlume,materialLiane;
 
 	// Use this for initialization
-	void Start () {
-	
+	void Start () {/*
+		rendererRoche = GuiRoche.GetComponent<GUITexture> ();
+		rendererBois = GuiBois.GetComponent<GUITexture> ();
+		rendererOs = GuiOs.GetComponent<GUITexture> ();
+		rendererMetal = GuiMetal.GetComponent<GUITexture> ();
+		rendererPlume = GuiPlume.GetComponent<GUITexture> ();
+		rendererLiane = GuiLiane.GetComponent<GUITexture> ();*/
 	}
 	
 	// Update is called once per frame
@@ -18,18 +29,99 @@ public class PlayerInventoryMaterials : MonoBehaviour {
 		
 	}
 
-	public int GetBoisCount()
+	public void AddItem(GameObject item)
 	{
-		return materialBois;
+		switch(item.name)
+		{
+			case "Roche" : 
+				if (nbRoche<3)
+				{
+					nbRoche++;
+					nbItem++;
+					//texteRoche.text = nbRoche.ToString ();
+				}
+			break;
+			case "Bois" :
+				if (nbBois<3)
+				{
+					nbBois++;
+					nbItem++;
+					//texteBois.text = nbBois.ToString ();
+				}
+				break;
+			case "Os" :
+				if (nbOs<3)
+				{
+					nbOs++;
+					nbItem++;
+					//texteOs.text = nbOs.ToString ();
+				}
+				break;
+			case "Metal" : 
+				if (nbMetal<3)
+				{
+					nbMetal++;
+					nbItem++;
+					//texteMetal.text = nbMetal.ToString ();
+				}
+				break;
+			case "Plume" : 
+				if (nbPlume<3)
+				{
+					nbPlume++;
+					nbItem++;
+					//textePlume.text = nbPlume.ToString ();
+				}
+				break;
+			case "Liane" : 
+			if (nbLiane<3)
+				{
+					nbLiane++;
+					nbItem++;
+					//texteLiane.text = nbLiane.ToString ();
+				}
+				break;
+		}
 	}
 
-	public int GetRocheCount()
+	public bool UseItem()
 	{
-		return materialRoche;
+		if(nbItem==0)
+			return false;
+		else
+		{
+			gameObject.GetComponent <PlayerController>().GetItems(nbRoche, nbBois, nbOs, nbMetal, nbPlume, nbLiane);
+			return true;
+		}
 	}
 
-	public int GetCordeCount()
+	public void setRoche(int roche)
 	{
-		return materialCorde;	
+		nbRoche = roche;
+	}
+
+	public void setBois(int bois)
+	{
+		nbBois = bois;
+	}
+
+	public void setOs(int os)
+	{
+		nbOs = os;
+	}
+
+	public void setMetal(int metal)
+	{
+		nbMetal = metal;
+	}
+
+	public void setPlume(int plume)
+	{
+		nbPlume = plume;
+	}
+
+	public void setLiane(int liane)
+	{
+		nbLiane = liane;
 	}
 }
