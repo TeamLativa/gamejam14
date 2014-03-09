af3143231d04786586f05e2095feb41eb3051c2e
dada9109 <david.gagne0@gmail.com>
2014-03-09 09:57:40 -0400

    totem works.. again

--- Assets/Scripts/NonPhysicsPlayerController.cs
+++ Assets/Scripts/NonPhysicsPlayerController.cs
@@ -30,6 +30,7 @@ public class NonPhysicsPlayerController : MonoBehaviour
 
 
 	private int nbRoche, nbBois,nbOs,nbMetal,nbPlume,nbLiane;
+	private int neededRoche, neededBois,neededOs,neededMetal,neededPlume,neededLiane;
 	private bool putRoche, putBois, putOs, putMetal, putPlume, putLiane;
 	private int nbParts = 6;
 	
@@ -45,6 +46,9 @@ public class NonPhysicsPlayerController : MonoBehaviour
 	
 	private float jumpTimer = 0.0f;
 	private float baseJumpHeight;
+
+	private bool onTotem;
+	private GameObject winner;
 
 	public bool IsStunned(){
 		return stunned;
@@ -77,7 +81,7 @@ public class NonPhysicsPlayerController : MonoBehaviour
 
 	void onTriggerEnterEvent( Collider2D col )
 	{
-		Debug.Log( "onTriggerEnterEvent: " + col.gameObject.name );
+		//Debug.Log( "onTriggerEnterEvent: " + col.gameObject.name );
 		if ( col.gameObject.tag == "ProjectileStunning")
 		{
 			col.gameObject.GetComponent<StunningProjectile>().Collision(gameObject);
@@ -87,12 +91,22 @@ public class NonPhysicsPlayerController : MonoBehaviour
 		{
 			col.gameObject.GetComponent<FlyingEnemyProj>().Collision(gameObject);
 		}
+
+		if (col.gameObject.tag == "Totem") 
+		{
+			onTotem= true;
+		}
 	}
 
 
 	void onTriggerExitEvent( Collider2D col )
 	{
-		Debug.Log( "onTriggerExitEvent: " + col.gameObject.name );
+		
+		if (col.gameObject.tag == "Totem") 
+		{
+			onTotem= false;
+		}
+	//	Debug.Log( "onTriggerExitEvent: " + col.gameObject.name );
 	}
 
 	#endregion
@@ -117,8 +131,15 @@ public class NonPhysicsPlayerController : MonoBehaviour
 		if(!stunned)
 			HandleMovement();
 
+		if( Input.GetButtonDown ("Xbutton_"+PNumber)&& onTotem)
+		{
+			checkDroppingItemOnTotem();
+		}
+
 		VerifySpeedTimer();
 		VerifyJumpTimer();
+
+		checkWinner ();
 	}
 
 	void VerifyJumpTimer()
@@ -230,7 +251,7 @@ public class NonPhysicsPlayerController : MonoBehaviour
 			
 		}
 	}
-	
+
 	public void ApplyBonusSpeed(float bonus, float time)
 	{
 		runSpeed *= bonus;
@@ -242,4 +263,238 @@ public class NonPhysicsPlayerController : MonoBehaviour
 		speedTimer = time;
 	}
 
+	void checkDroppingItemOnTotem()
+	{
+			gameObject.GetComponent<PlayerInventoryMaterials>().UseItem();
+			
+			//if(player got all items to add a part)
+			if((nbRoche==0)||(putRoche))
+			{
+				if((nbBois==0)||(putBois))
+				{
+					if((nbOs==0)||(putOs))
+					{
+						if((nbMetal==0)||(putMetal))
+						{
+							if((nbPlume==0)||(putPlume))
+							{
+								if(nbLiane!=0)
+								{	
+									if((!putLiane) && (nbLiane==neededLiane))
+									{
+										Instantiate(TotemPart1, SetPosition(false), Quaternion.identity);
+										putLiane = true;
+										removeOne("liane");
+										nbParts--;
+									}
+								}
+							}
+							else
+							{	
+								if((!putPlume) && (nbPlume==neededPlume))
+								{	
+									Instantiate(TotemPart2, SetPosition(true), Quaternion.identity);
+									putPlume = true;
+									removeOne("plume");
+									nbParts--;
+								}
+							}
+						}
+						else
+						{	
+							if(((!putMetal) && (nbMetal==neededMetal))
+							{
+								Instantiate(TotemPart3, SetPosition(false), Quaternion.identity);
+								putMetal = true;
+								removeOne("metal");
+								nbParts--;
+							}
+						}
+					}
+					else
+					{	
+						if((!putOs) && (nbOs==neededOs))
+						{
+							Instantiate(TotemPart4, SetPosition(false), Quaternion.identity);
+							putOs = true;
+							removeOne("os");
+							nbParts--;
+						}
+					}
+				}
+				else
+				{	
+					if((!putBois) && (nbBois==neededBois))
+					{
+						Instantiate(TotemPart5, SetPosition(false), Quaternion.identity);
+						putBois = true;
+						removeOne("bois");
+						nbParts--;
+					}
+				}
+			}
+			else
+			{	
+				if((!putRoche) && (nbRoche==neededRoche))
+				{
+					Instantiate(TotemPart6, SetPosition(false), Quaternion.identity);
+					putRoche = true;
+					removeOne("roche");
+					nbParts--;
+				}
+			}
+			
+			if((putRoche)&&(putBois)&&(putOs)&&(putMetal)&&(putPlume)&&(putLiane))
+			{
+
+				winner = this.gameObject;
+				
+				GameObject part;
+				part = (GameObject)Instantiate(TotemPart6, GetPosition ("Roche"), Quaternion.identity);
+				part.gameObject.transform.localScale = FlipTotem(part.gameObject.transform.localScale);
+				part = (GameObject)Instantiate(TotemPart5, GetPosition ("Bois"), Quaternion.identity);
+				part.gameObject.transform.localScale = FlipTotem(part.gameObject.transform.localScale);
+				part = (GameObject)Instantiate(TotemPart4, GetPosition ("Os"), Quaternion.identity);
+				part.gameObject.transform.localScale = FlipTotem(part.gameObject.transform.localScale);
+				part = (GameObject)Instantiate(TotemPart3, GetPosition ("Metal"), Quaternion.identity);
+				part.gameObject.transform.localScale = FlipTotem(part.gameObject.transform.localScale);
+				part = (GameObject)Instantiate(TotemPart1, GetPosition ("Liane"), Quaternion.identity);
+				part.gameObject.transform.localScale = FlipTotem(part.gameObject.transform.localScale);
+
+				if(winner.name == "Player 1")
+				{
+					part = (GameObject)Instantiate(TotemPart2, GetPosition ("PlumeG"), Quaternion.identity);
+					part.gameObject.transform.localScale = FlipTotem(part.gameObject.transform.localScale);
+				}
+				else if (winner.name=="Player 2")
+				{
+					part = (GameObject)Instantiate(TotemPart2, GetPosition ("PlumeD"), Quaternion.identity);
+					part.gameObject.transform.localScale = FlipTotem(part.gameObject.transform.localScale);
+				}
+
+				loadGameOver(winner.name);
+				
+			}
+
+	}
+
+	
+	Vector3 FlipTotem(Vector3 currentScale){
+		
+		Vector3 scale = currentScale;
+		scale.x *= -1;
+		return scale;
+	}
+
+
+	public void GetItems(int roche, int bois, int os, int metal, int plume, int liane)
+	{
+		nbRoche = roche;
+		nbBois = bois;
+		nbOs = os;
+		nbMetal = metal;
+		nbPlume = plume;
+		nbLiane = liane;
+	}
+	void removeOne(string objet)
+	{
+		switch(objet)
+		{
+		case "roche": nbRoche--; gameObject.GetComponent<PlayerInventoryMaterials>().setRoche(nbRoche);break;
+		case "bois":nbBois--; gameObject.GetComponent<PlayerInventoryMaterials>().setRoche(nbBois);break;
+		case "os":nbMetal--; gameObject.GetComponent<PlayerInventoryMaterials>().setOs(nbOs);break;
+		case "metal":nbMetal--; gameObject.GetComponent<PlayerInventoryMaterials>().setRoche(nbMetal);break;
+		case "plume":nbPlume--; gameObject.GetComponent<PlayerInventoryMaterials>().setRoche(nbPlume);break;
+		case "liane":nbLiane--; gameObject.GetComponent<PlayerInventoryMaterials>().setRoche(nbLiane);break;
+		}
+	}
+
+	void setParameterNeeded(int roche, int bois, int os, int metal, int plume ,int liane)
+	{
+		neededRoche = roche;
+		neededBois = bois;
+		neededOs = os;
+		neededMetal = metal;
+		neededPlume = plume;
+		neededLiane = liane;
+	}
+
+	Vector2 SetPosition(bool isItTheBird)
+	{
+		switch (nbParts) 
+		{
+		case 1 : 
+			if(isItTheBird)
+				return new Vector2(TotemPart2.transform.position.x,TotemPart1.transform.position.y); 
+			else 
+				return TotemPart1.transform.position; break;
+		case 2 : 
+			if(isItTheBird)
+				return TotemPart2.transform.position;
+			else 
+				return new Vector2(TotemPart1.transform.position.x,TotemPart2.transform.position.y); break;
+		case 3 : 
+			if(isItTheBird)
+				return new Vector2(TotemPart2.transform.position.x,TotemPart3.transform.position.y); 
+			else 
+				return TotemPart3.transform.position; break;
+		case 4 : 
+			if(isItTheBird)
+				return new Vector2(TotemPart2.transform.position.x,TotemPart4.transform.position.y); 
+			else 
+				return TotemPart4.transform.position; break;
+		case 5 :  
+			if(isItTheBird)
+				return new Vector2(TotemPart2.transform.position.x,TotemPart5.transform.position.y); 
+			else 
+				return TotemPart5.transform.position; break;
+		case 6 :  
+			if(isItTheBird)
+				return new Vector2(TotemPart2.transform.position.x,TotemPart6.transform.position.y); 
+			else 
+				return TotemPart6.transform.position; break;
+		default: return new Vector2(5,5); break;
+		}
+	}
+	
+	Vector2 GetPosition(string tag)
+	{
+		switch(tag)
+		{
+		case "Roche": return new Vector2(-GameObject.FindWithTag("Roche").transform.position.x,GameObject.FindWithTag("Roche").transform.position.y); break;
+		case "Bois": return new Vector2(-GameObject.FindWithTag("Bois").transform.position.x,GameObject.FindWithTag("Bois").transform.position.y); break;
+		case "Os": return new Vector2(-GameObject.FindWithTag("Os").transform.position.x,GameObject.FindWithTag("Os").transform.position.y);break;
+		case "Metal": return new Vector2(-GameObject.FindWithTag("Metal").transform.position.x,GameObject.FindWithTag("Metal").transform.position.y);break;
+		case "PlumeG": return new Vector2(-GameObject.FindWithTag("PlumeG").transform.position.x,GameObject.FindWithTag("PlumeG").transform.position.y);break;
+		case "PlumeD": return new Vector2(-GameObject.FindWithTag("PlumeD").transform.position.x,GameObject.FindWithTag("PlumeD").transform.position.y);break;
+		case "Liane": return new Vector2(-GameObject.FindWithTag("Liane").transform.position.x,GameObject.FindWithTag("Liane").transform.position.y);break;
+		default : return new Vector2(5,5);break;
+		}
+	}
+
+
+	void checkWinner()
+	{
+		if(winner!=null)
+		{
+			switch(winner.name)
+			{
+				case "Player 1" : destroyTotem("right");break;
+				case "Player 2" : destroyTotem("left");break;
+			}
+		}
+	}
+
+	void destroyTotem(string totemSideToDestroy)
+	{
+		if(totemSideToDestroy=="left")
+		Destroy (GameObject.FindWithTag("PlumeG"));
+		else if (totemSideToDestroy=="right")
+		Destroy (GameObject.FindWithTag("PlumeD"));
+	}
+
+	void loadGameOver(string winner)
+	{
+
+	}
 }
